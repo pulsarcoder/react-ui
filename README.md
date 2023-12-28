d29fac28ebd1c92ae762952754dce177f2f9a6e1
pulsarcoder <pulsarcoder@gmail.com>
2023-12-28 09:41:40 +0530

    updated with custom button component

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
+# Getting started
+
+## Start the app
+
+```bash
+npm start
+```
+
+## Start the storybook
+
+```bash
+npm run storybook
+```
+
+
 # UI Components
 
 * Table
---
+++ src/stories/CustomButton.stories.js
@@ -0,0 +1,23 @@
+import { Button } from '../shared/components/Button';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+export default {
+    title: 'React/UI/Custom Button',
+    component: Button,
+    parameters: {
+        // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+        layout: 'centered',
+    },
+    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+    tags: ['autodocs'],
+    // More on argTypes: https://storybook.js.org/docs/api/argtypes
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+};
+
+export const Primary = {
+    args: {
+        label: 'Button',
+    },
+};
